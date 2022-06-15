eaf10116676174caf3348cd01d1db707795e59bc
Niyah <73986655+Coningg@users.noreply.github.com>
2022-06-15 23:38:33 +0800

    SpeedComponent

--- Niyah'sMod/Niyah_sModClass.cs
+++ Niyah'sMod/Niyah_sModClass.cs
@@ -5,37 +5,87 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using MelonLoader;
+using VRC.SDKBase;
+using Il2CppSystem;
+
+using UnhollowerRuntimeLib;
 namespace Niyah_sMod
 {
     public class Niyah_sModClass : MelonMod
     {
-        float A = 1;
+        private float originalWalkSpeed;
+        private float originalRunSpeed;
+        private float originalStrafeSpeed;
+        private float speedMultiplier=1;
+        private int speedLevel = 0;
+
+        private void SetPlayerSpeed(VRCPlayerApi mySelf)
+        {
+            if (speedLevel >= 0)
+            {
+                speedMultiplier = speedLevel + 1;
+            }
+            else
+            {
+                speedMultiplier = 1 + speedLevel * 0.2f;
+            }
+            mySelf.SetWalkSpeed(originalWalkSpeed * speedMultiplier);
+            mySelf.SetRunSpeed(originalRunSpeed * speedMultiplier);
+            mySelf.SetStrafeSpeed(originalStrafeSpeed * speedMultiplier);
+            LoggerInstance.Msg("Current Speed Multiplier:"+ speedMultiplier);
+        }
         public override void OnUpdate()
         {
             if (Input.GetKeyDown(KeyCode.T))
             {
-
-            }
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-
-            }
-            if (Input.GetKeyDown(KeyCode.U))
-            {
-                int i = 0;
-                for(i=0;i<100;i++)
+                if (Networking.LocalPlayer != null)
                 {
-                    LoggerInstance.Msg(i);
-                }
-            }
-            if (Input.GetKeyDown(KeyCode.N))
-            {
+                    VRCPlayerApi player = Networking.LocalPlayer;
+                    ClassInjector.RegisterTypeInIl2Cpp<SpeedComponent>();
 
+                    if (player.gameObject.GetComponent<SpeedComponent>())
+                    {
+                    }
+                    else
+                    {
+                        player.gameObject.AddComponent<SpeedComponent>();
+                        speedLevel = 0;
+                        speedMultiplier = 1;
+                        originalWalkSpeed = player.GetWalkSpeed();
+                        originalRunSpeed = player.GetRunSpeed();
+                        originalStrafeSpeed = player.GetStrafeSpeed();
+                    }
+
+                    speedLevel = Mathf.Clamp(speedLevel + 1, -4, 4);         
+                    SetPlayerSpeed(player);
+                }
+
+            }
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                if (Networking.LocalPlayer != null)
+                {
+                    VRCPlayerApi player = Networking.LocalPlayer;
+                    ClassInjector.RegisterTypeInIl2Cpp<SpeedComponent>();
+                    if (player.gameObject.GetComponent<SpeedComponent>())
+                    {
+                    }
+                    else
+                    {
+                        player.gameObject.AddComponent<SpeedComponent>();
+                        speedLevel = 0;
+                        originalWalkSpeed = player.GetWalkSpeed();
+                        originalRunSpeed = player.GetRunSpeed();
+                        originalStrafeSpeed = player.GetStrafeSpeed();
+                    }
+                    speedLevel = Mathf.Clamp(speedLevel - 1, -4, 4);
+                    SetPlayerSpeed(player);
+                }
             }
         }
         public override void OnLateUpdate()
         {
-            
+
         }
 
         public override void OnFixedUpdate()
@@ -44,4 +94,12 @@ namespace Niyah_sMod
         }
 
     }
+
+    [RegisterTypeInIl2Cpp]
+    public class SpeedComponent : MonoBehaviour
+    {
+        public SpeedComponent(System.IntPtr ptr) : base(ptr) { }
+        public SpeedComponent() : base(ClassInjector.DerivedConstructorPointer<SpeedComponent>()) => ClassInjector.DerivedConstructorBody(this);
+    }
+
 }
